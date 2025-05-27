ebf4816af4fdd36172268b050457a8bc2fccf0ec
Gherex <129119400+Gherex@users.noreply.github.com>
2025-05-27 01:24:30 -0300

    Add files via upload

---
+++ target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO categories (name) VALUES ('Trabajo');
+INSERT INTO categories (name) VALUES ('Estudio');
+INSERT INTO categories (name) VALUES ('Salud');
+INSERT INTO categories (name) VALUES ('Finanzas');
+INSERT INTO categories (name) VALUES ('Deporte');
+INSERT INTO categories (name) VALUES ('Otro');
